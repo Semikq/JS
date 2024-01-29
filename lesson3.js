1a3fa75e9a5c587a128028630ce7e8d81679955c
Semikq <usern1861@gmail.com>
2024-01-29 14:47:52 +0200

    new

--- lesson3.js
+++ lesson3.js
@@ -1,101 +1,66 @@
 {
-    alert("Привітання користувача")
-    let korus =  prompt("Як тебе звати?")
-    alert(`Вітаю тебе - ${korus}`)
-}
-
-
-{
-    alert("Додавання блін")
-    let vod = prompt("Введіть слово")
-    res = vod.split(", ").join(",блін ")
-    alert("Введене ваше слово: " + res)
-}
-
-
-{
-    alert("Перетворіть рядок, введений користувачем, таким чином, щоб перша літера ставала великою, а решта - маленькими:")
-    let str = prompt("Введіть слово")
-    let result
-    let str1 = str.slice(0,1).toUpperCase()
-    let str2 = str.slice(1).toLowerCase()
-    result = str1 + str2
-    alert("Перша літера велика, інші маленькі: " + result)
-}
-
-
-
-{
-    alert("Порахуйте кількість слів у рядку. Використовуйте розбиття за пробілами. Довжина масиву називається так само, як і довжина рядка.")
-    let rech = prompt("Ваше слово");
-    let slov = rech.split(' '); 
-    let skiki_slov = slov.length;  
-    alert("Кількість слів: " + skiki_slov);
-}
-
-
-{
-    alert("ПІБ було з великої літери, а решта - маленькі")
-    let im = prompt("Яке у тебе ім'я?");
-    let pr = prompt("Яке у тебе прізвище?");
-    let bat = prompt("Як тебе по-батькові?");
-    let fullName;
-    im,pr,bat.trim();
-    im1 = im.slice(0,1).toUpperCase();
-    im2 = im.slice(1).toLowerCase();
-    pr1 = pr.slice(0,1).toUpperCase();
-    pr2 = pr.slice(1).toLowerCase();
-    bat1 = bat.slice(0,1).toUpperCase();
-    bat2 = bat.slice(1).toLowerCase();
-    fullName = (im1 + im2) + (pr1 + pr2) + (bat1 + bat);
-    alert(fullName);
-}
-
-
-{
-    alert("Не використовуючи .replace замініть слово пиво у рядку на слово чай")
-    alert("Було жарко. Василь пив пиво вприкуску з креветками")
-    let str = "Було жарко. Василь пив пиво вприкуску з креветками"
-    let result
-    result = str.split("пиво").join("чай")
-    alert(result)
-}
-
-
-{
-    alert("Знайдіть у рядку HTML тег. Видаліть, тобто виріжте його - створіть інший рядок, в якому будуть всі символи до тега і після нього Тег може бути будь-яким. split і join не використовуйте, натомість використовуйте indexOf і slice. .replace не використовуйте")
-    let str = "якийсь текст, в якому є один тег <br/> і всяке інше";
-    let result
-    let teg = str.indexOf('<');
-    let end = str.indexOf('>', teg);
-    result = str.slice(0, teg) + str.slice(end + 1);
-    alert(result); 
+    const user = prompt("What's your name?")
+    alert(`Hello ${user}`)
 }
 
 {
-    alert("На основі попереднього завдання зробіть тег великими літерами. replace, split і join не використовуйте, натомість використовуйте indexOf і slice,")
-let str = "якийсь текст у якому є один тег <br/> і всяке інше";
-let start = str.indexOf('<');
-let end = str.indexOf('>', start);
-let result =(str.slice(0, start) + str.slice(start, end).toUpperCase() + str.slice(end));
-alert(result);
+    let user = prompt("Enter the word please")
+    user = user.split(", ").join(", shit ")
+    console.log(user)
 }
 
 {
-    alert("Попросіть користувача ввести рядок черезprompt. prompt не дозволяє вводити рядкові рядки. Дамо користувачеві таку можливість - Користувач може вводити \n як маркер нового рядка.")
-    let korus = prompt()
-    let res = korus.split("\\n ").join("\n ")
-    alert(`Ваш рядок ${res}`)
+    const str = "cANBerRa"
+    const result = str.slice(0,1).toUpperCase() + str.slice(1).toLowerCase()
+    console.log(result)
 }
 
 {
-    alert("Нехай користувач введе будь-який текст із посиланням на youtube. Використовуючи регулярний вираз вийміть із посилання ідентифікатор видосу і створіть рядок із вбудованим блоком HTML.")
-    const reg_sulka = (/http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/)
-    let korus_sulka = prompt()
-    let id = korus_sulka.match(reg_sulka)[1]
-    const sulka = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" ></iframe>`;
-    document.write(sulka)
+    let word = prompt()
+    word = word.split(" ")
+    console.log(word.length)
 }
 
+{
+    function words(word){
+        word = word.trim()
+        return word = word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase()
+    }
+    const name = words(prompt("name"))
+    const surname = words(prompt("surname"))
+    const fatherName = words(prompt("fatherName"))
+    const fullName = `${name} ${surname} ${fatherName}`
+    console.log(fullName)
+}
 
+{
+    const str = "It was hot. Vasyl drank beer with shrimps"
+    const result = str.split("beer").join("tea")
+    console.log(result) 
+}
 
+{
+    const str = "some text that has one <br/> tag and everything else"
+    const result = str.slice(0,str.indexOf("<")) + str.slice(str.indexOf(">") + 2)
+    console.log(result)
+}
+
+{
+    const str = "some text that has one <br/> tag and everything else"
+    const result = str.slice(0,str.indexOf("<")) + str.slice(str.indexOf("<"),str.indexOf(">") + 2).toUpperCase() + str.slice(str.indexOf(">") + 2)
+    console.log(result)
+}
+
+{
+    let user = prompt("Please enter a sentence with \\n")
+    user = user.split("\\n ").join("\n")
+    console.log(user)
+}
+
+{
+    const regul = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/
+    const user = prompt()
+    const id = user.match(regul)[1]
+    const result = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}"></iframe>`
+    document.write(res)
+}
--- lesson4.js
+++ lesson4.js
@@ -1,87 +1,63 @@
-let zadania
-if(zadania = prompt("Обери завдання: ")){
-
-if(zadania === "one")
 {
-    alert("Number: odd")
-    let a = prompt("Введіть число: ")
-    if(parseInt(a))
-    {
-    if(a % 2 === 0){
-        alert("Число парне")
+    const number = prompt()
+    if(!isNaN(number)){
+        if(number % 2 === 0){
+        console.log("The number is even")
+        }else{
+        console.log("The number is not even")
     }
-    else{
-        alert("Число непарне")
-    }
-    }
-    else{
-        alert("Число не коректне")
+    }else{
+        console.log("This is a string")
     }
 }
 
-if(zadania === "two")
 {
-    alert("String: lexics")
-    let text = prompt("Введіть текст:")
-    let slovo = "слово"
-    
-    if (text.indexOf(slovo) !== -1) {
-      alert("Знайдено некоректне слово у тексті")
-    } else {
-      alert("Текст введено без некоректних слів")
+    const text = prompt("Enter your sentence")
+    if(!(text.includes("word"))){
+    console.log("All is well")
+    }else{
+    console.log("An incorrect word was found")
     }
 }
 
-if(zadania === "three")
-    {
-        alert("Boolean")
-        let a = confirm("У тебе сьогодні добрий настрій?")
-        let b = confirm("Ти сьогодні поїв?")
-        let c = confirm("Сьогодні був чудовий день?")
-        alert(a)
-        alert(b)
-        alert(c)
+{
+    const one = confirm("Is the weather good today?")
+    const two = confirm("Are you OK?")
+    const three = confirm("Did you have a good day?")
+    console.log(one,two,three)
 }
 
-if(zadania === "four")
 {
-    alert("Boolean: if")
-    if(confirm("Вкажіть:\nТак,ви чоловік\nНі,ви жінка")){
-        alert("Ви чоловік")
-    }
-    else{
-        alert("Ви жінка")
+    const gender = confirm("If you are male, click Yes, if you are female, click No.")
+    if(gender === true){
+    alert("You are a man")
+    }else{
+    alert("You are a woman")
     }
 }
 
-if(zadania === "five")
 {
-    alert("Comparison: sizes")
-let rozmer = prompt("Введіть розмір одягу Італійська:")
-let italy = {
- 38: "S",
- 40: "M",
- 44: "L",
- 48: "XL",
- 52: "XXL"
-}
-if (italy[rozmer]) {
-  alert("Розмір одягу в американській системі: " + italy[rozmer])
-} else {
-  alert("Такого розміру одягу немає")
-}
+    const size = prompt()
+        if(+size === 38){
+            alert("S")
+        }else if(+size === 40){
+            alert("M")
+        }else if(+size === 44){
+            alert("L")
+        }else if(+size === 48){
+            alert("XL")
+        }else if(+size === 52){
+            alert("XXL")
+        }else{
+            alert("This size does not exist")
+        }
 }
 
-if(zadania === "six")
 {
-    alert("Ternary")
-    let a =confirm("Вкажіть:\nТак,ви чоловік\nНі,ви жінка") ? "Ви чоловік" : "Ви жінка"
-    alert(a)
+    confirm("If you are male, click Yes, if you are female, click No.")?alert("You are a man"):alert("You are a woman")
 }
 
-if(zadania === "seven")
 {
-    alert("Training")
 // bool type cast
 !!2 //True
 !!0 //False
@@ -98,13 +74,13 @@ if(zadania === "seven")
 0 && 1 && 2 //0
 2 || 1 || 0 //2
 2 && 1 && 0 //0
-confirm('left') || confirm('right') //Якщо Ок - то програма закінчується, якщо скасувати то right
-confirm('left') && confirm('right') //Якщо Ок - right, якщо скасувати то нічого
+confirm('left') || confirm('right') //If OK, the program ends, if canceled, then correct
+confirm('left') && confirm('right') //If ok - correct, if you cancel, then nothing
 //null, undefined, so on
 null || 2 //2
 undefined && 1 //undefined
 alert("Hello") && confirm('Are you sexy?'); //Hello
-alert("Hello") || confirm('Are you drunk?'); //Hello,потім обираємо відповідь в confirm
+alert("Hello") || confirm('Are you drunk?'); //Hello, then select an answer in the confirm
 //brackets and complex expressions
 (undefined || 2) && (3 || 0) //3
 (2 && 1) || (null && 0) //1
@@ -120,176 +96,139 @@ parseInt("0") ? 'true' : 'false' //False
 (-1 + 1) && "zero" //0
 "-1" + 1 && "oups" //"oups"
 (typeof null === 'object') ? "null is object" : "null is null" //"null is object"
-// ternary && || //?
+// ternary && ||
 Math.random() < 0.5 && 'less' || 'more' //0.6 < 0.5 =  'more', 0.4 < 0.5 = 'less'
 let b = (a = Math.random()) < 0.5 && 'less: '+a || 'more: '+a //0.6 < 0.5 = 'less: '+a
 //in for array
 [2,3,5,7,11].indexOf(7) > -1 ? 'prime' : 'not found' //prime
 }
 
-if(zadania === "eight")
 {
-alert("Prompt: or");
-let vik = +prompt("Введіть свій вік") || null;
-let rik = vik ? 2023 - vik : null;
-alert(vik ? "Ваш рік: " + rik : "Ви не ввели свій вік");
+    const age = prompt() || null
+    const result = age ? 2024 - age : null
+    alert(result ? "Date of birth " + result : "You have not entered anything")
 }
 
-if(zadania === "nine")
 {
-    alert("Confirm: or this days")
-confirm('шопінг?') || alert("ти - бяка")
+    confirm("Shopping?") || alert("You are crazy")
 }
 
-if(zadania === "ten")
 {
-    alert("Confirm: if this days")
-    let a = confirm('шопінг?')
-    if(a === true){
-    }
-    else{
-        alert("ти - бяка")
+    if(confirm("Shopping?")){
+        alert("Come on")
+    }else{
+        alert("You are crazy")
     }
 }
 
-if(zadania === "eleven")
 {
-    alert("Default: or")
-    let P = prompt("Введіть прізвище:") || "Ivanov"
-    let N = prompt("Введіть ім'я:") || "Ivan"
-    let B = prompt("Введіть по батькові:") || "Ivanovich"
-    let PI = P ? P : "Ivanov"
-    let NA = N ? N : "Ivan"
-    let BA = B ? B : "Ivanovich"
-    alert(PI + " " + NA + " " + BA)
+    const name = prompt() || "Ivan"
+    const surname = prompt() || "Ivanov"
+    const fatherName = prompt() || "Ivanovich"
+    console.log(name,surname,fatherName)
 }
 
-if(zadania === "twelve")
 {
-alert("Default: if")
-let N = prompt("Введіть ім'я:");
-let P = prompt("Введіть прізвище:");
-let B = prompt("Введіть по батькові:");
-
-if (N === "" || N === null) {
-  N = "Ivan";
+    let name = prompt() 
+    let surname = prompt() 
+    let fatherName = prompt()
+    if(!name.trim()){
+        name = "Ivan"
+    }
+    if(!surname.trim()){
+        surname = "Ivanov"
+    }
+    if(!fatherName.trim()){
+        fatherName = "Ivanovich"
+    }
+    console.log(name, surname, fatherName)
 }
 
-if (P === "" || P === null) {
-  P = "Ivanov";
-}
-
-if (B === "" || B === null) {
-  B = "Ivanovich";
-}
-
-alert(P + " " + N + " " + B);
-}
-
-if(zadania === "thirteen")
 {
-    alert("Login and password")
-    let login = prompt("Введіть логін: ")
-    if(login === "admin")
-    {
-        let password = prompt("Введіть пароль: ")
-            if(password === "qwerty")
-            {
-                alert("Успішний вхід")
+    const login = prompt("Enter your login")
+        if(login === "admin"){
+            const password = prompt("Enter your password")
+            if(password === "qwerty"){
+                alert("Welcome")
+            }else{
+                alert("Invalid password")
             }
-            else{
-                alert("Неправельний пароль")
+        }else{
+            alert("Invalid login")
+        }
+}
+
+{
+    const buyOrSell = confirm("buy or sell")
+        if(buyOrSell){
+            const currency = prompt("Choose the currency you want to buy for UAH. Choice between: usd or eur")
+            if(currency.toLowerCase() === "usd"){
+                const rate = prompt("Exchange rate 1 Usd = 34 UAH\nHow much do you want to buy?")
+                if(!isNaN(rate) && rate !== null && rate.trim() !== ""){
+                    const result = rate * 34
+                    alert(result)
+                }else{
+                    alert("You entered wrong numbers or nothing")
+                }
+            }else if(currency.toLowerCase() === "eur"){
+                const rate = prompt("Exchange rate 1 Eur = 38 UAH\nHow much do you want to buy?")
+                if(!isNaN(rate) && rate !== null && rate.trim() !== ""){
+                    const result = rate * 38
+                    alert(result)
+                }else{
+                    alert("You entered wrong numbers or nothing")
+                }
+            }else{
+                alert("There is no such currency")
             }
-    }
-
-    else
-    {
-        alert("Неправельний логін")
-    }
+        }else{
+            const currency = prompt("Choose the currency you want to sell to receive UAH.  Choice between: usd or eur")
+            if(currency.toLowerCase() === "usd"){
+                const rate = prompt("Exchange rate 1 Usd = 38 UAH\nDo you want to sell?")
+                if(!isNaN(rate) && rate !== null && rate.trim() !== ""){
+                    const result = rate * 38
+                    alert(result)
+                }else{
+                    alert("You entered wrong numbers or nothing")
+                }
+            }else if(currency.toLowerCase() === "eur"){
+                const rate = prompt("Exchange rate 1 Eur = 42 UAH\nDo you want to sell?")
+                if(!isNaN(rate) && rate !== null && rate.trim() !== ""){
+                    const result = rate * 42
+                    alert(result)
+                }else{
+                    alert("You entered wrong numbers or nothing")
+                }
+            }else{
+                alert("There is no such currency")
+            }
+        }
 }
 
-if(zadania === "fourteen")
 {
-    alert("Currency exchange")
-    let valyta 
-    if(valyta = prompt("Оберіть валюту\nusd - Долари\neur - Євро\nfynt - Фунт стерлінга"))
+    const user = prompt()
+        let robot = Math.ceil(Math.random() * 3)
+        if(robot === 1){
+            robot = "Stone"
+        }else if(robot === 2){
+            robot = "Paper"
+        }else if(robot === 3){
+            robot = "Scissors"
+        }
+
+        if(user === robot){
+            alert("Draw")
+        }else if((user === "Stone" && robot === "Scissors") ||(user === "Scissors" && robot === "Paper") ||(user === "Paper" && robot === "Stone")){
+            alert("You win")
+        }else{
+            alert("Robot win")
+        }
+}
+
 {
-
-    if(valyta.toLowerCase() === "usd")
-    {
-        let usd = prompt("Введіть Долари")
-        let rate= confirm("Купити") || alert("Продати")
-        if(rate === true){
-            rate = usd * 2
-            alert(rate)
-        }
-        else{
-            rate = usd * 1.5
-            alert(rate)
-        }
-    }
-
-    if(valyta.toLowerCase() === "eur")
-    {
-        let eur = prompt("Введіть Євро")
-        let rate = confirm("Купити") || alert("Продати")
-        if(rate === true){
-            rate = eur * 3
-            alert(rate)
-        }
-        else{
-            rate = eur * 2.5
-            alert(rate)
-        }
-    }
-
-    if(valyta.toLowerCase() === "fynt")
-    {
-        let fynt = prompt("Введіть Фунт стерлінга")
-        let rate = confirm("Купити") || alert("Продати")
-        if(rate === true){
-            rate = fynt * 5
-            alert(rate)
-        }
-        else{
-            rate = fynt * 4.5
-            alert(rate)
-        }
-    }
-}
-    else{
-        alert("Такої валюти немає, спробуйте іншу")
-    }s
-}
-
-if(zadania === "fifteen")
-{
-alert("Scissors")
-let korus = prompt("Введіть свій вибір: камінь, ножиці чи папір").toLowerCase()
-
-let random = Math.floor(Math.random() * 3)
-let komp
-
-if (random === 0) {
-    komp = "камінь"
-} else if (random === 1) {
-    komp = "ножиці"
-} else {
-    komp = "папір"
-}
-
-alert("Вибір комп'ютера: " + komp)
-
-if (korus === komp) {
-  alert("Нічия")
-} else if (
-  (korus === "камінь" && komp === "ножиці") ||
-  (korus === "ножиці" && komp === "папір") ||
-  (korus === "папір" && komp === "камінь")
-) {
-  alert("Ви перемогли")
-} else {
-  alert("Комп'ютер переміг")
-}
-}
+    const user = prompt()
+        let robot = Math.ceil(Math.random() * 3)
+        robot = (robot === 1) ? "Stone" : robot === 2 ? "Paper" : "Scissors"
+        const result = (user === robot) ? "Draw" : ((user === "Stone" && robot === "Scissors") || (user === "Scissors" && robot === "Paper") || (user === "Paper" && robot === "Stone")) ? "You win" : "Robot win";
+        alert(result)
 }
